a1433f74bf946e306fa740bb7005e1a657b4369c
Ayan <ayanestlycode@gmail.com>
2022-09-26 23:01:26 -0500

    woohoooo built out the functions

--- programs/myepicproject1/src/lib.rs
+++ programs/myepicproject1/src/lib.rs
@@ -1,15 +1,59 @@
 use anchor_lang::prelude::*;
+// This is kinda like an import statement, allowing us to import anchors tools
 
 declare_id!("6YJvRpLhVBZfmLxHy5m3P2Nv61A25H4cGmVnE2BxMsdF");
-
+//this is a macro, dexlaring what the program's account id is and should be used at the root of all anchor programs
 #[program]
+//this is an attribute macro, it attaches code to the modules and lets anchor know this is our programs instruction handlers
+//this allows you to call your solana program from the dapp via fetch request
+
 pub mod myepicproject1 {
+    //pub mod is a module (collection of functions and vars, pub makes it public)
     use super::*;
-    pub fn start_stuff_off(ctx: Context <StartStuffOff>) -> Result <()
-> {
-    Ok(())
-}}
+    //brings all the things imported from the anchor prelude into the current modules context
+    //pub fn - the function sso is a handler with context as a param and outputs result
 
+    pub fn start_stuff_off(ctx: Context <StartStuffOff>) -> Result <()> {
+        let base_account = &mut ctx.accounts.base_account;
+        //&mut allows to get a mutable ref to baseacc, allowing you to make changes
+        base_account.total_gifs = 0;
+        //just initializing the totalgifs
+        Ok(())
+    }
+    
+
+    pub fn add_gif(ctx: Context<AddGif>) -> Result <()> {
+        let base_account = &mut ctx.accounts.base_account;
+        base_account.total_gifs += 1;
+        Ok(())
+
+    }
+}
+
+ 
 #[derive(Accounts)]
-pub struct StartStuffOff {}
+pub struct StartStuffOff<'info> {
+  #[account(init, payer = user, space = 9000)]
+    //change 9000 to 10000 if error, this is the bytes of space for your account
 
+    //initializing baseaccounts
+  pub base_account: Account<'info, BaseAccount>,
+
+  #[account(mut)]
+  pub user: Signer <'info>,
+    //this is data passed into program that orives to the program that the user calling this actually owns their wallet
+
+  pub system_program: Program <'info, System>,
+    //this is a ref to the system program, a program that creators of solana depoloyed that other programs can talk to
+}
+//this is a derive macro that has a param of accounts, defines account constraints 
+#[derive(Accounts)]
+pub struct AddGif<'info> {
+  #[account(mut)]
+  pub base_account: Account<'info, BaseAccount>,
+}
+
+#[account]
+pub struct BaseAccount {
+    pub total_gifs: u64,
+}
--- tests/myepicproject1.js
+++ tests/myepicproject1.js
@@ -1,16 +1,42 @@
 const anchor = require("@project-serum/anchor");
+const { SystemProgram } = anchor.web3;
+
 
 const main = async() => {
   console.log("Starting test...")
-
-  anchor.setProvider(anchor.AnchorProvider.env());
-  const program = anchor.workspace.Myepicproject1;
   
-  const transaction = await program.rpc.startStuffOff(); 
-  console.log("Your transaction string: ", transaction)
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
+  const baseAccount = anchor.web3.Keypair.generate();
+  const program = anchor.workspace.Myepicproject1;
+
+
+  let tx = await program.rpc.startStuffOff({
+    accounts: {
+      baseAccount: baseAccount.publicKey,
+      user: provider.wallet.publicKey,
+      systemProgram: SystemProgram.programId,
+    },
+    signers: [baseAccount],
+
+  })
+  console.log("Your transaction string: ", tx);
+
+  let account = await program.account.baseAccount.fetch(baseAccount.publicKey)
+  console.log("Gif Count:", account.totalGifs.toString())
+
+  await program.rpc.addGif({
+    accounts: {
+      baseAccount: baseAccount.publicKey,
+    },
+  });
+  
+  account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+  console.log("Gif count", account.totalGifs.toString())
 }
 
 const runMain = async () => {
+  //run main is a functional comp that gets your main function to run async using try catch
   try{
     await main();
     process.exit(0);
